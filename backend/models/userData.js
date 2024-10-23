5d746f23bbc754cf269d1798a4908f04a11b84b3
Shibu Joseph <shibujoseph1815@gmail.com>
2024-10-23 16:01:13 +0530

    Users data db setup

---
+++ backend/models/userData.js
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  temperature_unit: {
+    type: DataTypes.ENUM('Celsius', 'Fahrenheit'),
+    allowNull: false,
+    defaultValue: 'Celsius' // Default to Celsius
+  }
+}, {
+  tableName: 'Users',
+  timestamps: true // You can choose to include timestamps if you want to track creation/update times
+});
+
+export default User;
