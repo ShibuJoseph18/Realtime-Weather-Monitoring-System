c82e895f462e71d63a65a1f5e06acbd17c250123
Shibu Joseph <shibujoseph1815@gmail.com>
2024-10-26 19:33:44 +0530

    Activate Schedulers

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import sequelize from './config/database.js';
-// import './scheduler/weatherRealTime.scheduler.js'; // Ensure the scheduler is imported
-// import './scheduler/transferWeatherData.scheduler.js'; // Ensure the scheduler is imported
+import './scheduler/weatherRealTime.scheduler.js'; // Ensure the scheduler is imported
+import './scheduler/transferWeatherData.scheduler.js'; // Ensure the scheduler is imported
 // import './scheduler/checkThreshold.scheduler.js';
-import createThreshold from './routes/userThreshold.routes.js';
+// import createThreshold from './routes/userThreshold.routes.js';
 import getCities from './routes/getCities.routes.js';
 // import users from './routes/users.routes.js';
 
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000; // Use environment variable for PORT
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', createThreshold);
+// app.use('/api', createThreshold);
 app.use('/api', getCities);
 // app.use('/api', users);
 
